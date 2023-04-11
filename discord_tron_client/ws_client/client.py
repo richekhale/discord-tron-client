e034798f30e0f7b0647bf9faba62bae3d22beec4
richekhale <32632599+richekhale@users.noreply.github.com>
2023-04-11 10:22:08 -0700

    Fix syntax
    Signed-off-by: bghira <bghira@users.github.com>

--- discord_tron_client/ws_client/client.py
+++ discord_tron_client/ws_client/client.py
@@ -52,7 +52,7 @@ async def websocket_client(config: AppConfig, startup_sequence:str = None):
                     logging.debug(f"{message}")
                     payload = json.loads(message)
                     async with semaphore:
-                        asyncio.create_task(processor.process_command_with_semaphore(processor, semaphore, payload=payload, websocket=websocket))
+                        asyncio.create_task(process_command_with_semaphore(processor, semaphore, payload=payload, websocket=websocket))
         except asyncio.exceptions.IncompleteReadError as e:
             logging.warning(f"IncompleteReadError: {e}")
             # ... handle the situation as needed
