fd2930b4112d98a1cdc68257480d88e6e9f4ad8d
richekhale <32632599+richekhale@users.noreply.github.com>
2023-04-19 19:43:08 -0700

    Embed the url inside the post
    Signed-off-by: bghira <bghira@users.github.com>

---
+++ discord_tron_client/classes/api_client.py
@@ -0,0 +1,66 @@
+import logging, json, requests, sys, os, io
+from discord_tron_client.classes.auth import Auth
+from discord_tron_client.classes.app_config import AppConfig
+from PIL import Image
+
+class ApiClient:
+    def __init__(self, auth: Auth, config: AppConfig):
+        self.auth = auth
+        self.config = config
+        self.base_url = config.get_master_url()
+        self.verify_ssl = config.verify_master_ssl()
+        self.api_key = config.get_master_api_key()
+        self.headers = self._set_auth_header()
+        
+    def get(self, endpoint: str, params: dict = None):
+        if params is None:
+            params = {}
+        url = self.base_url + endpoint
+        params["api_key"] = self.api_key
+        params["access_token"] = self.auth.get()
+        response = requests.get(url, params=params, verify=self.verify_ssl)
+        return self.handle_response(response)
+
+    def put(self, endpoint: str, params: dict = None):
+        if params is None:
+            params = {}
+        url = self.base_url + endpoint
+        params["api_key"] = self.api_key
+        params["access_token"] = self.auth.get()
+        response = requests.put(url, params=params, verify=self.verify_ssl)
+        return self.handle_response(response)
+    
+    def post(self, endpoint: str, params: dict = None, files: dict = None):
+        if params is None:
+            params = {}
+        headers = self._set_auth_header()
+        url = self.base_url + endpoint
+        response = requests.post(url, params=params, verify=self.verify_ssl, files=files, headers=headers)
+        return self.handle_response(response)
+    
+    def send_file(self, endpoint: str, file_path: str):
+        with open(file_path, "rb") as f:
+            response = requests.post(endpoint, files={"file": f})
+        return response
+    
+    def send_pil_image(self, endpoint: str, image: Image):
+        buffer = io.BytesIO()
+        image.save(buffer, format="PNG")
+        buffer.seek(0)
+        response = self.post(endpoint, files={"image": buffer})
+        return response
+    
+    def send_buffer(self, endpoint: str, buffer: io.BytesIO):
+        response = self.post(endpoint, files={"file": buffer})
+        return response
+    
+    def handle_response(self, response):
+        if response.status_code == 200:
+            return response.json()
+        else:
+            raise Exception("Error: {}".format(response.text))
+    
+    def _set_auth_header(self) -> dict:
+        # We need the token from self.auth.get() to be set as the Authorization header using the Bearing token type
+        current_ticket = self.auth.get()
+        return { "Authorization": f"Bearer {current_ticket['access_token']}" }
---
+++ discord_tron_client/classes/uploader.py
@@ -0,0 +1,20 @@
+# Upload files to the central API.
+from PIL import Image
+from discord_tron_client.classes.auth import Auth
+from discord_tron_client.classes.app_config import AppConfig
+from discord_tron_client.classes.api_client import ApiClient
+import logging, json
+
+class Uploader:
+    def __init__(self, api_client: ApiClient, config: AppConfig):
+        self.api_client = api_client
+        self.config = config
+        
+    async def image(self, image: Image):
+        logging.debug(f"Uploading image to {self.config.get_master_url()}")
+        result = self.api_client.send_pil_image('/upload_image', image)
+        logging.debug(f"Image uploader received result: {result}")
+        
+        if "image_url" in result:
+            return result["image_url"]
+        raise Exception(f"Image upload failed: {result}")
