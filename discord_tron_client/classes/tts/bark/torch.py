2dbb43e7700678507a8fe3618ae39d7b1cad9dbd
richekhale <32632599+richekhale@users.noreply.github.com>
2023-05-02 14:05:03 -0700

    Allow character voices within prompts to change
    Signed-off-by: bghira <bghira@users.github.com>

--- discord_tron_client/classes/tts/bark/torch.py
+++ discord_tron_client/classes/tts/bark/torch.py
@@ -99,7 +99,7 @@ class BarkTorch:
     def generate_long(self, prompt, user_config):
         # Split the prompt into smaller segments
         segments, _ = self.split_text_prompt(prompt)
-        return self.generate_long_from_segments(segments, user_config)       
+        return self.generate_long_from_segments(segments, user_config)
 
     def generate_long_from_segments(self, prompts: List[str], user_config):
         # Generate audio for each prompt
