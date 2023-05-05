5e5b9430ab4efb23e784dc835cd69550be62993d
richekhale <32632599+richekhale@users.noreply.github.com>
2023-05-05 15:51:19 -0700

    Fix for Vicuna
    Signed-off-by: bghira <bghira@users.github.com>

--- discord_tron_client/classes/llm/stable_vicuna/torch.py
+++ discord_tron_client/classes/llm/stable_vicuna/torch.py
@@ -26,7 +26,7 @@ class StableVicunaTorch:
     def _predict(self, user_config, prompt, history = None, seed = 1337, max_tokens = 512, temperature = 0.8, repeat_penalty = 1.1, top_p = 0.95, top_k=40):
         return predict.generate(tokenizer=self.tokenizer, model=self.vicuna, user_config=user_config, user_prompt=prompt, max_tokens=max_tokens, temperature=temperature, top_p=top_p, top_k=top_k, seed=seed)
 
-    def predict(self, prompt, user_config, max_tokens = 4096, temperature = 1.0, repeat_penalty = 1.1, top_p = 0.95, top_k=40, history = None):
+    def predict(self, prompt, history, user_config, max_tokens = 4096, temperature = 1.0, repeat_penalty = 1.1, top_p = 0.95, top_k=40):
         logging.debug(f"Begin StableVicuna prediction routine")
         logging.debug(f"Our received parameters: max_tokens {max_tokens} top_p {top_p} top_k {top_k} repeat_penalty {repeat_penalty} temperature {temperature}")
         time_begin = time.time()
