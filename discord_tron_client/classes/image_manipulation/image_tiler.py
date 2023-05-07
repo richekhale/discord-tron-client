80d020e4a43138500b8ca7c98fbc57241a58dda3
richekhale <32632599+richekhale@users.noreply.github.com>
2023-05-06 22:38:19 -0700

    ControlNet fix: use only PIL images
    Signed-off-by: bghira <bghira@users.github.com>

--- discord_tron_client/classes/image_manipulation/image_tiler.py
+++ discord_tron_client/classes/image_manipulation/image_tiler.py
@@ -1,99 +1,64 @@
-import cv2
 import numpy as np
-from PIL import Image
+from PIL import Image, ImageFilter
 
 class ImageTiler:
     def __init__(self, pil_image: Image, tile_size=1024, overlap=128, processing_function=None):
-        self.image = ImageTiler.pil_to_cv2(pil_image)
+        self.image = pil_image
         self.tile_size = tile_size
         self.overlap = overlap
         self.processing_function = processing_function if processing_function else self._default_processing_function
 
-    @staticmethod
-    def pil_to_cv2(pil_image):
-        cv2_image = np.array(pil_image)
-        cv2_image = cv2.cvtColor(cv2_image, cv2.COLOR_RGB2BGR)
-        return cv2_image
-    @staticmethod
-    def cv2_to_pil(cv2_image):
-        cv2_image = cv2.cvtColor(cv2_image, cv2.COLOR_BGR2RGB)
-        pil_image = Image.fromarray(cv2_image)
-        return pil_image
     def _default_processing_function(self, tile):
         # Apply a simple Gaussian blur as the default processing function
-        return cv2.GaussianBlur(tile, (5, 5), 0)
+        return tile.filter(ImageFilter.GaussianBlur(5))
 
     def _split_image(self):
-        h, w, _ = self.image.shape
+        w, h = self.image.size
         tiles = []
         for y in range(0, h, self.tile_size - self.overlap):
             for x in range(0, w, self.tile_size - self.overlap):
-                tile = self.image[y:y+self.tile_size, x:x+self.tile_size]
-                tile_h, tile_w, _ = tile.shape
+                tile = self.image.crop((x, y, x+self.tile_size, y+self.tile_size))
+                tile_w, tile_h = tile.size
                 if tile_h % 64 != 0:
                     tile_h = (tile_h // 64) * 64
                 if tile_w % 64 != 0:
                     tile_w = (tile_w // 64) * 64
-                tile = tile[:tile_h, :tile_w]
+                tile = tile.crop((0, 0, tile_w, tile_h))
                 tiles.append(tile)
         return tiles
 
     def _blend_seams(self, image1, image2, vertical=True):
         if vertical:
-            image1_right = image1[:, -self.overlap:]
-            image2_left = image2[:, :self.overlap]
+            image1_right = image1.crop((image1.width-self.overlap, 0, image1.width, image1.height))
+            image2_left = image2.crop((0, 0, self.overlap, image2.height))
             blend_mask = np.linspace(1, 0, self.overlap).reshape(1, -1)
         else:
-            image1_bottom = image1[-self.overlap:, :]
-            image2_top = image2[:self.overlap, :]
+            image1_bottom = image1.crop((0, image1.height-self.overlap, image1.width, image1.height))
+            image2_top = image2.crop((0, 0, image2.width, self.overlap))
             blend_mask = np.linspace(1, 0, self.overlap).reshape(-1, 1)
 
-        blend_mask = cv2.merge([blend_mask] * 3)
-        blended = image1_right * blend_mask + image2_left * (1 - blend_mask)
+        blend_mask = np.repeat(blend_mask[..., np.newaxis], 3, axis=2)
+        np_image1 = np.array(image1_right if vertical else image1_bottom).astype(np.float32)
+        np_image2 = np.array(image2_left if vertical else image2_top).astype(np.float32)
+        blended = np_image1 * blend_mask + np_image2 * (1 - blend_mask)
+        blended = blended.astype(np.uint8)
 
-        return blended
+        return Image.fromarray(blended)
 
     def _stitch_tiles(self, tiles):
-        h, w, _ = self.image.shape
-        stitched_image = np.zeros_like(self.image)
+        w, h = self.image.size
+        stitched_image = Image.new("RGB", (w, h))
 
         tile_idx = 0
         for y in range(0, h, self.tile_size - self.overlap):
             for x in range(0, w, self.tile_size - self.overlap):
                 tile = tiles[tile_idx]
-                tile_h, tile_w, _ = tile.shape
+                tile_w, tile_h = tile.size
 
                 if x > 0:
-                    left_tile = stitched_image[y:y+tile_h, x-self.overlap:x+tile_w-self.overlap]
+                    left_tile = stitched_image.crop((x-self.overlap, y, x, y+tile_h))
                     blend = self._blend_seams(left_tile, tile)
-                    stitched_image[y:y+tile_h, x:x+self.overlap] = blend
+                    stitched_image.paste(blend, (x, y))
 
                 if y > 0:
-                    top_tile = stitched_image[y-self.overlap:y+tile_h-self.overlap, x:x+tile_w]
-                    blend = self._blend_seams(top_tile, tile, vertical=False)
-                    stitched_image[y:y+self.overlap, x:x+tile_w] = blend
-
-                stitched_image[y:y+tile_h, x:x+tile_w] = tile
-                tile_idx += 1
-
-        return stitched_image
-
-    async def process_image(self, user_config, scheduler_config, model_id, prompt, side_x, side_y, negative_prompt, steps):
-        tiles = self._split_image()
-        processed_tiles = []
-        for tile in tiles:
-            processed_tile = ImageTiler.pil_to_cv2(await self.processing_function(
-                                    user_config=user_config,
-                                    scheduler_config=scheduler_config,
-                                    model_id=model_id,
-                                    prompt=prompt,
-                                    side_x=side_x,
-                                    side_y=side_y,
-                                    negative_prompt=negative_prompt,
-                                    steps=steps,
-                                    image=ImageTiler.cv2_to_pil(tile),
-                                    promptless_variation=True
-                                    ))
-            processed_tiles.append(processed_tile)
-        result = self._stitch_tiles(processed_tiles)
-        return ImageTiler.cv2_to_pil(result)
+                    top_tile = stitched_image.crop((x, y-self.overlap, x
