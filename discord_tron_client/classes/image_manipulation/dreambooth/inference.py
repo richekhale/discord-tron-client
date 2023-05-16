1c6890b0475a02dd470aaf6bbcd7eec3db46c2fd
richekhale <32632599+richekhale@users.noreply.github.com>
2023-05-16 11:45:44 -0700

    Dreambooth: WIP
    Signed-off-by: bghira <bghira@users.github.com>

--- discord_tron_client/classes/image_manipulation/dreambooth/inference.py
+++ discord_tron_client/classes/image_manipulation/dreambooth/inference.py
@@ -1,21 +1,30 @@
 from accelerate import Accelerator
-from diffusers import DiffusionPipeline
-
+from diffusers import DiffusionPipeline, UNet2DConditionModel
+from transformers import CLIPTextModel
+import torch, xformers
+torch.manual_seed(420)
 # Load the pipeline with the same arguments (model, revision) that were used for training
 model_id = "junglerally/digital-diffusion"
-pipeline = DiffusionPipeline.from_pretrained(model_id)
-accelerator = Accelerator()
-# Use text_encoder if `--train_text_encoder` was used for the initial training
-unet, text_encoder = accelerator.prepare(pipeline.unet, pipeline.text_encoder)
-# Restore state from a checkpoint path. You have to use the absolute path here.
-accelerator.load_state("/sddata/dreambooth/daruma-v2-1/checkpoint-100")
+checkpoints = [ "1500" ]
+for checkpoint in checkpoints:
+    if checkpoint != "0":
+        unet = UNet2DConditionModel.from_pretrained(f"/notebooks/images/datasets/models/checkpoint-{checkpoint}/unet")
+        # if you have trained with `--args.train_text_encoder` make sure to also load the text encoder
+        text_encoder = CLIPTextModel.from_pretrained(f"/notebooks/images/datasets/models/checkpoint-{checkpoint}/text_encoder")
+        pipeline = DiffusionPipeline.from_pretrained(model_id, unet=unet, text_encoder=text_encoder)
+    else:
+        pipeline = DiffusionPipeline.from_pretrained(model_id)
+    pipeline.to("cuda")
 
-# Rebuild the pipeline with the unwrapped models (assignment to .unet and .text_encoder should work too)
-pipeline = DiffusionPipeline.from_pretrained(
-    model_id,
-    unet=accelerator.unwrap_model(unet),
-    text_encoder=accelerator.unwrap_model(text_encoder),
-)
+    # Perform inference, or save, or push to the hub
+    pipeline.save_pretrained("dreambooth-pipeline")
+    negative = "low quality, low res, messy, grainy, smooth, sand, big eyes, anime, fractured, cracked, wrinkles, makeup (deformed, distorted, disfigured:1.3), poorly drawn, bad anatomy, wrong anatomy, extra limb, missing limb, floating limbs, (mutated hands and fingers:1.4), disconnected limbs, mutation, mutated, ugly, disgusting, blurry, amputation, synthetic, rendering"
+    output = pipeline(negative_prompt=negative, prompt="a puppy, hanging out on the beach", num_inference_steps=35).images[0]
 
-# Perform inference, or save, or push to the hub
-pipeline.save_pretrained("pseudo-test")
+    output.save(f'/notebooks/test1-{checkpoint}.png')
+    output = pipeline(negative_prompt=negative, prompt="a woman, hanging out on the beach", num_inference_steps=35).images[0]
+
+    output.save(f'/notebooks/test2-{checkpoint}.png')
+    output = pipeline(negative_prompt=negative, prompt="a woman with areolas, hanging out on the beach", num_inference_steps=35).images[0]
+
+    output.save(f'/notebooks/test3-{checkpoint}.png')
--- discord_tron_client/classes/image_manipulation/dreambooth/training.sh
+++ discord_tron_client/classes/image_manipulation/dreambooth/training.sh
@@ -10,14 +10,18 @@ accelerate launch train_dreambooth.py \
   --class_data_dir=$CLASS_DIR \
   --output_dir=$OUTPUT_DIR \
   --with_prior_preservation --prior_loss_weight=1.0 \
-  --instance_prompt="$2" \
-  --class_prompt="$2" \
+  --instance_prompt="a woman with " \
+  --class_prompt="digital art portrait of a person" \
   --resolution=768 \
-  --train_batch_size=8 \
-  --gradient_accumulation_steps=2 --gradient_checkpointing \
+  --train_batch_size=2 \
   --use_8bit_adam \
-  --learning_rate=5e-6 \
+  --learning_rate=7e-7 \
   --lr_scheduler="constant" \
   --lr_warmup_steps=0 \
-  --num_class_images=200 \
-  --max_train_steps=1500
+  --num_class_images=1500 \
+  --max_train_steps=300 \
+  --mixed_precision=bf16 \
+  --allow_tf32 \
+  --checkpointing_steps=10 \
+  --resume_from_checkpoint="checkpoint-500"
+#--train_text_encoder \
