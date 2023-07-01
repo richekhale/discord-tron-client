d28147be9604120fceae30681f7aac2ade15355d
richekhale <32632599+richekhale@users.noreply.github.com>
2023-07-01 15:06:03 -0700

    Do not compile for SDXL yet.

--- discord_tron_client/classes/image_manipulation/diffusion.py
+++ discord_tron_client/classes/image_manipulation/diffusion.py
@@ -243,7 +243,7 @@ class DiffusionPipelineManager:
                     logging.error(f"Could not enable CPU offload on the model: {e}")
             torch._dynamo.config.suppress_errors = True
             torch._dynamo.config.log_level = logging.WARNING
-            if "sdxl" not in model_id:
+            if "ptx0/s" not in model_id:
                 self.pipelines[model_id].unet = torch.compile(self.pipelines[model_id].unet, mode="reduce-overhead", fullgraph=True)
             else:
                 logging.warning(f'Not enabling fullgraph for {model_id}')
