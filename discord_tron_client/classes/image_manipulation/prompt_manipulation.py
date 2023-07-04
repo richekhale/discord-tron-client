9915472763c6c6cff77e0de5d1c9f408e0418a4f
richekhale <32632599+richekhale@users.noreply.github.com>
2023-07-04 09:42:30 -0700

    Compel: detect and configure for dual encoder pipeline

--- discord_tron_client/classes/image_manipulation/prompt_manipulation.py
+++ discord_tron_client/classes/image_manipulation/prompt_manipulation.py
@@ -5,35 +5,53 @@ import torch
 
 # Manipulating prompts for the pipeline.
 class PromptManipulation:
-    def __init__(self, pipeline, device):
+    def __init__(self, pipeline, device, use_second_encoder: bool = False):
+        self.is_valid_pipeline(pipeline)
         self.pipeline = pipeline
-        if not hasattr(self.pipeline, "tokenizer") and not hasattr(self.pieline, "tokenizer_2"):
-            raise Exception(
-                f"Cannot use PromptManipulation on a model without a tokenizer."
-            )
-        pipe_tokenizer = self.pipeline.tokenizer
-        if self.pipeline.tokenizer is None and hasattr(self.pipeline, 'tokenizer_2'):
+        if (use_second_encoder and self.has_dual_text_encoders(pipeline)) or (
+            self.has_dual_text_encoders(pipeline) and self.is_sdxl_refiner(pipeline)
+        ):
+            # SDXL Refiner and Base can both use the 2nd tokenizer/encoder.
             pipe_tokenizer = self.pipeline.tokenizer_2
-
-        pipe_text_encoder = self.pipeline.text_encoder
-        if self.pipeline.text_encoder is None and hasattr(self.pipeline, 'text_encoder_2'):
             pipe_text_encoder = self.pipeline.text_encoder_2
+        else:
+            # Any other pipeline uses the first tokenizer/encoder.
+            pipe_tokenizer = self.pipeline.tokenizer
+            pipe_text_encoder = self.pipeline.text_encoder
+
         self.compel = Compel(
             tokenizer=pipe_tokenizer,
             text_encoder=pipe_text_encoder,
             truncate_long_prompts=False,
-            device="cuda"
+            device="cuda",
         )
 
+    def has_dual_text_encoders(self, pipeline):
+        return hasattr(pipeline, "text_encoder_2")
+
+    def is_sdxl_refiner(self, pipeline):
+        # SDXL Refiner has the 2nd text encoder, only.
+        if self.pipeline.tokenizer is None and hasattr(self.pipeline, "tokenizer_2"):
+            return True
+        return False
+
+    def is_valid_pipeline(self, pipeline):
+        if not hasattr(self.pipeline, "tokenizer") and not hasattr(
+            self.pipeline, "tokenizer_2"
+        ):
+            raise Exception(
+                f"Cannot use PromptManipulation on a model without a tokenizer."
+            )
+
     def process(self, prompt: str):
-        conditioning = self.compel.build_conditioning_tensor(prompt)
+        conditioning = self.compel.build_conditioning_tensor(prompt, use_penultimate_clip_layer=True)
         return conditioning
 
     def process_long_prompt(self, positive_prompt: str, negative_prompt: str):
         if positive_prompt == "":
             positive_prompt = "since you did not provide a string, i will do it for you"
-        conditioning = self.compel.build_conditioning_tensor(positive_prompt)
-        negative_conditioning = self.compel.build_conditioning_tensor(negative_prompt)
+        conditioning = self.compel.build_conditioning_tensor(positive_prompt, use_penultimate_clip_layer=True)
+        negative_conditioning = self.compel.build_conditioning_tensor(negative_prompt, use_penultimate_clip_layer=True)
         [
             conditioning,
             negative_conditioning,
