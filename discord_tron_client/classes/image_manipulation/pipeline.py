f95bb1938cc113e652dba396992602c9413b0e7b
richekhale <32632599+richekhale@users.noreply.github.com>
2023-07-04 21:26:18 -0700

    Loop through multiple images for the refiner

--- discord_tron_client/classes/image_manipulation/pipeline.py
+++ discord_tron_client/classes/image_manipulation/pipeline.py
@@ -207,7 +207,6 @@ class PipelineRunner:
             batch_size = 2
         if hardware.should_sequential_offload():
             batch_size = 1
-        batch_size = 1
         try:
             alt_weight_algorithm = user_config.get("alt_weight_algorithm", False)
             use_latent_result = user_config.get('latent_refiner', True)
@@ -253,12 +252,10 @@ class PipelineRunner:
                     ).images
                 if use_latent_result:
                     preprocessed_images = self._refiner_pipeline(
-                        image=preprocessed_images,
+                        images=preprocessed_images,
                         user_config=user_config,
-                        generator=generator,
                         prompt=positive_prompt,
                         negative_prompt=negative_prompt,
-                        random_seed=True
                     )
                 new_image = self._controlnet_all_images(preprocessed_images=preprocessed_images, user_config=user_config, generator=generator)
             elif not upscaler and not promptless_variation and image is not None:
@@ -444,7 +441,7 @@ class PipelineRunner:
         ).images[0]
         return new_image
 
-    def _refiner_pipeline(self, image: Image, user_config: dict, generator, prompt: str = None, negative_prompt: str = None, random_seed = False):
+    def _refiner_pipeline(self, images: Image, user_config: dict, prompt: str = None, negative_prompt: str = None, random_seed = False):
         
         # Get the image width/height from 'image' if it's provided
         logging.info(
@@ -454,19 +451,21 @@ class PipelineRunner:
         if prompt is None:
             prompt = user_config["tile_positive"]
             negative_prompt = user_config["tile_negative"]
-        # if random_seed:
-        #     generator = torch.Generator(device=self.pipeline_manager.device).manual_seed(-1)
-        return pipe(
-            prompt=prompt,
-            negative_prompt=negative_prompt,
-            image=image,
-            guidance_scale=user_config.get("refiner_guidance", 7.5),
-            strength=user_config.get("refiner_strength", 0.3),
-            aesthetic_score=user_config.get("aesthetic_score", 5.0),
-            negative_aesthetic_score=user_config.get("negative_aesthetic_score", 1.0),
-            generator=generator,
-            num_inference_steps=user_config.get("refiner_steps", 10),
-        ).images
+        generator = self._get_generator(user_config=user_config)
+        new_images = []
+        for image in images:
+            new_images.append(pipe(
+                prompt=prompt,
+                negative_prompt=negative_prompt,
+                image=image,
+                guidance_scale=user_config.get("refiner_guidance", 7.5),
+                strength=user_config.get("refiner_strength", 0.3),
+                aesthetic_score=user_config.get("aesthetic_score", 5.0),
+                negative_aesthetic_score=user_config.get("negative_aesthetic_score", 1.0),
+                generator=generator,
+                num_inference_steps=user_config.get("refiner_steps", 10),
+            ).images)
+        return new_images
 
 
     def _controlnet_all_images(self, preprocessed_images: list, user_config: dict, generator):
