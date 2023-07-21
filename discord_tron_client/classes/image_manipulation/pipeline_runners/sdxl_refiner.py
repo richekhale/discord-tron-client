2f9443ec087750d2c1676e7a93227773e090353f
richekhale <32632599+richekhale@users.noreply.github.com>
2023-07-21 14:57:06 -0700

    [WIP] text2img compact version

--- discord_tron_client/classes/image_manipulation/pipeline_runners/sdxl_refiner.py
+++ discord_tron_client/classes/image_manipulation/pipeline_runners/sdxl_refiner.py
@@ -8,20 +8,20 @@ class SdxlRefinerPipelineRunner(BasePipelineRunner):
         self.pipeline = pipeline
 
     def __call__(self, **args):
-        # Set all defaults at once
         user_config = args.get("user_config", None)
-        del args["user_config"] # This doesn't get passed to Diffusers.
+        del args["user_config"]  # This doesn't get passed to Diffusers.
 
         # Currently, it seems like the refiner's prompt weighting is broken.
         # We are disabling it by default.
-        if user_config is not None and user_config.get(
-            "refiner_prompt_weighting", False
-        ):
-            args[
-                "num_images_per_prompt"
-            ] = 1  # SDXL, when using prompt embeds, only generates 1 image per prompt.
-            return self.pipeline(**args).images
+        if user_config is not None and user_config.get("refiner_prompt_weighting", False):
+            # SDXL, when using prompt embeds, must be instructed to only generate 1 image per prompt.
+            args["num_images_per_prompt"] = 1
+            # We don't pass the text prompts in, when using embeds.
+            for unwanted_arg in ["prompt", "negative_prompt"]:
+                if unwanted_arg in args:
+                    del args[unwanted_arg]
         else:
+            # If we're not using prompt embeds, delete them from the arguments.
             for unwanted_arg in [
                 "prompt_embeds",
                 "negative_prompt_embeds",
@@ -30,4 +30,5 @@ class SdxlRefinerPipelineRunner(BasePipelineRunner):
             ]:
                 if unwanted_arg in args:
                     del args[unwanted_arg]
-            return self.pipeline(**args).images
+        
+        return self.pipeline(**args).images
--- discord_tron_client/classes/image_manipulation/pipeline_runners/text2img.py
+++ discord_tron_client/classes/image_manipulation/pipeline_runners/text2img.py
@@ -3,50 +3,30 @@ from discord_tron_client.classes.image_manipulation.pipeline_runners.base_runner
 class Text2ImgPipelineRunner(BasePipelineRunner):
     def __init__(self, pipeline):
         self.pipeline = pipeline
-    def __call__(
-        self,
-        prompt: str,
-        negative_prompt: str,
-        user_config: dict,
-        prompt_embeds,
-        negative_prompt_embeds,
-        width,
-        height,
-        num_inference_steps,
-        denoising_end,
-        guidance_scale,
-        guidance_rescale,
-        num_images_per_prompt,
-        output_type,
-        generator,
-        pooled_prompt_embeds = None,
-        negative_pooled_prompt_embeds = None,
-    ):
+
+    def __call__(self, **args):
+        # Get user_config and delete it from args, it doesn't get passed to the pipeline
+        user_config = args.get("user_config", None)
+        del args["user_config"] 
+
         if user_config.get("prompt_weighting", True):
-            return self.pipeline(
-                prompt_embeds=prompt_embeds,
-                negative_prompt_embeds=negative_prompt_embeds,
-                num_images_per_prompt=num_images_per_prompt,
-                height=height,
-                width=width,
-                num_inference_steps=int(float(num_inference_steps)),
-                guidance_scale=float(guidance_scale),
-                guidance_rescale=float(guidance_rescale),
-                denoising_end=denoising_end,
-                output_type=output_type,
-                generator=generator,
-            ).images
+            # Remove unwanted arguments for this condition
+            for unwanted_arg in ["prompt", "negative_prompt"]:
+                if unwanted_arg in args:
+                    del args[unwanted_arg]
         else:
-            return self.pipeline(
-                prompt=prompt,
-                negative_prompt=negative_prompt,
-                num_images_per_prompt=num_images_per_prompt,
-                height=height,
-                width=width,
-                num_inference_steps=int(float(num_inference_steps)),
-                guidance_rescale=int(guidance_rescale),
-                guidance_scale=float(guidance_scale),
-                denoising_end=denoising_end,
-                output_type=output_type,
-                generator=generator
-            ).images
+            # Remove unwanted arguments for this condition
+            for unwanted_arg in ["prompt_embeds", "negative_prompt_embeds"]:
+                if unwanted_arg in args:
+                    del args[unwanted_arg]
+
+        # Convert specific arguments to desired types
+        if "num_inference_steps" in args:
+            args["num_inference_steps"] = int(float(args["num_inference_steps"]))
+        if "guidance_scale" in args:
+            args["guidance_scale"] = float(args["guidance_scale"])
+        if "guidance_rescale" in args:
+            args["guidance_rescale"] = float(args["guidance_rescale"])
+        
+        # Call the pipeline with arguments and return the images
+        return self.pipeline(**args).images
