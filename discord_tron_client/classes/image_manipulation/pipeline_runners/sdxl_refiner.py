07451d86a25a593123326db7abc07746e619cb4e
richekhale <32632599+richekhale@users.noreply.github.com>
2023-07-24 14:31:54 -0700

    DiffusionPipelineManager: Logging for SDXL Refiner args

--- discord_tron_client/classes/image_manipulation/pipeline_runners/sdxl_refiner.py
+++ discord_tron_client/classes/image_manipulation/pipeline_runners/sdxl_refiner.py
@@ -1,3 +1,4 @@
+import logging
 from discord_tron_client.classes.image_manipulation.pipeline_runners import (
     BasePipelineRunner,
 )
@@ -10,10 +11,11 @@ class SdxlRefinerPipelineRunner(BasePipelineRunner):
     def __call__(self, **args):
         user_config = args.get("user_config", None)
         del args["user_config"]  # This doesn't get passed to Diffusers.
-
+        logging.debug(f'Args (minus user_config) for SDXL refiner: {args}')
         # Currently, it seems like the refiner's prompt weighting is broken.
         # We are disabling it by default.
         if user_config is not None and user_config.get("refiner_prompt_weighting", True):
+            logging.info(f'Using SDXL prompt weighting.')
             # SDXL, when using prompt embeds, must be instructed to only generate 1 image per prompt.
             args["num_images_per_prompt"] = 1
             # We don't pass the text prompts in, when using embeds.
@@ -21,6 +23,7 @@ class SdxlRefinerPipelineRunner(BasePipelineRunner):
                 if unwanted_arg in args:
                     del args[unwanted_arg]
         else:
+            logging.info(f'Using prompt and negative_prompt strings.')
             # If we're not using prompt embeds, delete them from the arguments.
             for unwanted_arg in [
                 "prompt_embeds",
@@ -31,6 +34,7 @@ class SdxlRefinerPipelineRunner(BasePipelineRunner):
                 if unwanted_arg in args:
                     del args[unwanted_arg]
             if type(args['prompt'] != list):
+                logging.debug(f'Received non-list prompts. Encapsulating in list..')
                 args['prompt'] = [args['prompt']] * len(args['image'])
                 args['negative_prompt'] = [args['negative_prompt']] * len(args['image'])
         return self.pipeline(**args).images
