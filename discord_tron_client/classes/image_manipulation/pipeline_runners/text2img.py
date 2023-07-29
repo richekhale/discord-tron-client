a3829c7e15fdcf522042425dc101e7dc7295f3c9
richekhale <32632599+richekhale@users.noreply.github.com>
2023-07-28 22:52:34 -0700

    Logging

--- discord_tron_client/classes/image_manipulation/pipeline_runners/text2img.py
+++ discord_tron_client/classes/image_manipulation/pipeline_runners/text2img.py
@@ -1,4 +1,7 @@
 from discord_tron_client.classes.image_manipulation.pipeline_runners.base_runner import BasePipelineRunner
+from discord_tron_client.classes.app_config import AppConfig
+import logging
+config = AppConfig()
 
 class Text2ImgPipelineRunner(BasePipelineRunner):
     def __init__(self, pipeline):
@@ -12,16 +15,18 @@ class Text2ImgPipelineRunner(BasePipelineRunner):
             del args["pooled_prompt_embeds"]
         if "negative_pooled_prompt_embeds" in args:
             del args["negative_pooled_prompt_embeds"]
-        if user_config.get("prompt_weighting", True):
+        if user_config.get("prompt_weighting", True) and config.enable_compel():
             # Remove unwanted arguments for this condition
             for unwanted_arg in ["prompt", "negative_prompt"]:
                 if unwanted_arg in args:
                     del args[unwanted_arg]
+            logging.debug(f'Removing unwanted args: {unwanted_arg}')
         else:
             # Remove unwanted arguments for this condition
             for unwanted_arg in ["prompt_embeds", "negative_prompt_embeds"]:
                 if unwanted_arg in args:
                     del args[unwanted_arg]
+            logging.debug(f'Removing unwanted args: {unwanted_arg}')
 
         # Convert specific arguments to desired types
         if "num_inference_steps" in args:
@@ -31,4 +36,5 @@ class Text2ImgPipelineRunner(BasePipelineRunner):
         if "guidance_rescale" in args:
             args["guidance_rescale"] = float(args["guidance_rescale"])
         # Call the pipeline with arguments and return the images
+        logging.debug(f'Calling pipeline with args: {args}')
         return self.pipeline(**args).images
