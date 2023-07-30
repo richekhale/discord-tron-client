a165cd534b93cebad0355b7f3f4efb62a8f4ae42
richekhale <32632599+richekhale@users.noreply.github.com>
2023-07-30 14:26:02 -0700

    Bark: use No voice if default/none is given

--- discord_tron_client/classes/tts/bark/torch.py
+++ discord_tron_client/classes/tts/bark/torch.py
@@ -37,11 +37,9 @@ class BarkTorch:
 
     def _generate(self, prompt, user_config, character_voice: str = None):
         # generate audio from text
-        if character_voice is None:
-            logging.debug(f"No voice provided, using default.")
-            character_voice = user_config.get("tts_voice", "en_female_intense")
+        if character_voice is None or character_voice == 'none' or character_voice == 'default':
+            character_voice = None
         logging.debug(f"Generating text {prompt[32:]}.. with voice {character_voice}")
-
         audio = generate_audio(prompt, history_prompt=character_voice)
         return audio, None
 
