0d8999da388f0d91f8114b416f13b346a80d6abf
richekhale <32632599+richekhale@users.noreply.github.com>
2024-01-10 11:27:18 -0600

    Discord print_prompt should use a more relaxed length limit

--- discord_tron_client/message/discord.py
+++ discord_tron_client/message/discord.py
@@ -112,7 +112,7 @@ class DiscordMessage(WebsocketMessage):
             execute_time = round(execute_duration, 2)
         try:
             return (
-                f"**<@{author_id}>'s Prompt**: {prompt[:32]}...\n"
+                f"**<@{author_id}>'s Prompt**: {prompt[:255]}...\n"
                 f"**Seed**: `!seed {seed}`, `!guidance {user_config['guidance_scaling']}`, `!settings guidance_rescale {guidance_rescale}`, `!steps {steps}`, `!settings strength {strength}`\n"
                 f"**Model**: `{model_id}`\n"
                 f"**SDXL Refiner**: {latent_refiner}\n"
