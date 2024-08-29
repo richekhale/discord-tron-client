f3e1f4e8c80a820097825920827e687eddb92388
richekhale <32632599+richekhale@users.noreply.github.com>
2024-08-29 14:46:07 -0600

    fix quirk

--- discord_tron_client/message/discord.py
+++ discord_tron_client/message/discord.py
@@ -118,7 +118,7 @@ class DiscordMessage(WebsocketMessage):
         
         flux_adapter = user_config.get('flux_adapter_1')
         if "black-forest-labs" in model_id and flux_adapter:
-            model_id = f"!model {model_id}`, **Flux Capacitor** `!settings flux_adapter_1 {flux_adapter}`"
+            model_id = f"!model {model_id}`, **Flux Capacitor** `!settings flux_adapter_1 {flux_adapter}"
 
         guidance_rescale = user_config.get("guidance_rescale")
         if latent_refiner == "On":
