f0f0a7054ff0adeadebaadbe0cb10cda72769cad
richekhale <32632599+richekhale@users.noreply.github.com>
2024-12-23 18:27:16 -0600

    fix sd3 defaults

--- discord_tron_client/classes/image_manipulation/pipeline_runners/sd3_runner.py
+++ discord_tron_client/classes/image_manipulation/pipeline_runners/sd3_runner.py
@@ -15,6 +15,7 @@ class SD3PipelineRunner(BasePipelineRunner):
         user_config = args.get("user_config", None)
         del args["user_config"]
         args["skip_guidance_layers"] = [7, 8, 9]
+        args["max_sequence_length"] = 154
         # Use the prompt parameters to override args now
         args.update(prompt_parameters)
         logging.debug(f"Args (minus user_config) for SD3: {args}")
@@ -37,6 +38,14 @@ class SD3PipelineRunner(BasePipelineRunner):
             args["num_inference_steps"] = int(float(args["num_inference_steps"]))
         if "guidance_scale" in args:
             args["guidance_scale"] = float(args["guidance_scale"])
+        if "max_sequence_length" in args:
+            args["max_sequence_length"] = int(float(args["max_sequence_length"]))
+        if "skip_guidance_start" in args:
+            args["skip_layer_guidance_start"] = float(args["skip_layer_guidance_start"])
+        if "skip_layer_guidance_end" in args:
+            args["skip_layer_guidance_end"] = float(args["skip_layer_guidance_end"])
+        if "skip_layer_guidance_scale" in args:
+            args["skip_layer_guidance_end"] = float(args["skip_layer_guidance_scale"])
 
 
         self.apply_adapters(user_config)
