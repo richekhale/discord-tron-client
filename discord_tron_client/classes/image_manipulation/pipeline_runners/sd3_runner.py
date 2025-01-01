d3987871b4d658874e7e7ebd2cb4fce984fbb9f5
richekhale <32632599+richekhale@users.noreply.github.com>
2025-01-01 09:02:53 -0600

    sd3 tweaks

--- discord_tron_client/classes/image_manipulation/pipeline_runners/sd3_runner.py
+++ discord_tron_client/classes/image_manipulation/pipeline_runners/sd3_runner.py
@@ -3,18 +3,22 @@ from discord_tron_client.classes.image_manipulation.pipeline_runners import (
     BasePipelineRunner,
 )
 from discord_tron_client.classes.app_config import AppConfig
-
 config = AppConfig()
 
 
 class SD3PipelineRunner(BasePipelineRunner):
     def __call__(self, **args):
+        
         args["prompt"], prompt_parameters = self._extract_parameters(args["prompt"])
 
         # Get user_config and delete it from args, it doesn't get passed to the pipeline
         user_config = args.get("user_config", None)
         del args["user_config"]
-        args["skip_guidance_layers"] = [7, 8, 9]
+        args["skip_guidance_layers"] = user_config.get("skip_guidance_layers", -1)
+        if args["skip_guidance_layers"] == -1:
+            # set the true default
+            args["skip_guidance_layers"] = [7, 8, 9]
+            
         args["max_sequence_length"] = 154
         # Use the prompt parameters to override args now
         args.update(prompt_parameters)
@@ -46,9 +50,26 @@ class SD3PipelineRunner(BasePipelineRunner):
             args["skip_layer_guidance_end"] = float(args["skip_layer_guidance_end"])
         if "skip_layer_guidance_scale" in args:
             args["skip_layer_guidance_end"] = float(args["skip_layer_guidance_scale"])
+        if "skip_guidance_layers" in args and type(args["skip_guidance_layers"]) == str:
+            if args["skip_guidance_layers"].lower() == "none":
+                args["skip_guidance_layers"] = None
+            else:
+                try:
+                    args["skip_guidance_layers"] = [
+                        int(x) for x in args["skip_guidance_layers"].split(",")
+                    ]
+                except Exception as e:
+                    print(f"Error configuring SLG: {e}")
+                    args["skip_guidance_layers"] = None
 
 
         self.apply_adapters(user_config)
+        from diffusers import FlowMatchEulerDiscreteScheduler
+        self.pipeline.scheduler = FlowMatchEulerDiscreteScheduler.from_config(
+            user_config.get("model", "stabilityai/stable-diffusion-3.5-medium"),
+            subfolder="scheduler",
+            use_dynamic_shift=True,
+        )
 
         # Call the pipeline with arguments and return the images
         return self.pipeline(**args).images
